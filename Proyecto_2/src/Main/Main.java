7303061ba2de521acc73dfcb423c1b980c124ebe
r1c4rd0-b43t4 <r.baeta@correo.unimet.edu.ve>
2024-11-13 18:43:25 -0400

    reader arreglado y filtro de json

--- Proyecto_2/src/Main/Main.java
+++ Proyecto_2/src/Main/Main.java
@@ -1,27 +1,10 @@
 package Main;
-import Carga.*;
-        
+import Carga.Reader;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-/**
- *
- * @author rdbae
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Reader reader = new Reader();
         reader.Read();
-        
     }
-    
 }
--- Proyecto_2/src/Main/Nodo.java
+++ Proyecto_2/src/Main/Nodo.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Main;
 
-/**
- *
- * @author HP
- */
+
 public class Nodo <T> {
     private T valor;
     private Nodo <T> siguiente;
